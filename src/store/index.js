e935fd89bbc7d34c1bc85f80597d4e8c9318f3eb
jackplugged <sidneisimmon@gmail.com>
2018-08-11 13:52:30 -0300

    Estudo finalizado

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,19 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
 
 import rootReducer from "./reducers";
 
-const store = createStore(persistedReducer);
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["todos"]
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
